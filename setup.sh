7dd980cab6b8ec023ede5c714c99c08d054cc644
Jacob Mealey <jacobmealey.maine@gmail.com>
2020-02-10 20:54:14 -0500

    pretty shit

--- setup.sh
+++ setup.sh
@@ -1,5 +1,13 @@
+echo " _    ________  _______  ______"
+echo "| |  / /  _/  |/  / __ \/ ____/"
+echo "| | / // // /|_/ / /_/ / /     "
+echo "| |/ // // /  / / _, _/ /___   "
+echo "|___/___/_/  /_/_/ |_|\____/   "
 echo "Jacob Mealeys VIMRC configuration tool!"
+
+echo "Copying Files"
 cp vimrc ~/.vimrc
 cp -rf ./ ~/.vim
 
+echo "Compiling plugins"
 python3 ~/.vim/plugged/YouCompleteMe/install.py --clang-completer
